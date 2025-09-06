d71047a3d25295462b70dc03293687c3a0b13286
Rabie Otoum <rabie.amin.otoum@gmail.com>
2025-09-07 01:35:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+# setup.py
+from setuptools import setup, find_packages
 
+setup(
+    name="char_tokenizer",
+    version="0.1.0",
+    description="Character-level tokenizer and vocab utilities",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "pandas>=1.0.0"
+    ],
+    python_requires=">=3.8",
+)
