e972ecea047b634e54517408e4d2a829fd07ab57
Rabie Otoum <rabie.amin.otoum@gmail.com>
2025-09-07 01:46:58 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-# setup.py
 from setuptools import setup, find_packages
 
 setup(
-    name="char_tokenizer",
+    name="char-tokenizer",
     version="0.1.0",
-    description="Character-level tokenizer and vocab utilities",
+    description="Character-level tokenizer for NLP tasks",
+    author="Rabie Otoum",
+    author_email="rabie.amin.otoum@gmail.com",
+    url="https://github.com/rabie-amin/char_tokenizer_pkg",
     packages=find_packages(),
-    include_package_data=True,
     install_requires=[
-        "pandas>=1.0.0"
+        "pandas>=1.0",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.7",
 )
