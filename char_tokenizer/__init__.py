45429d937aad21240cd0ef45ff585044737ec432
Rabie Otoum <rabie.amin.otoum@gmail.com>
2025-09-07 01:33:21 +0300

    Update __init__.py

--- char_tokenizer/__init__.py
+++ char_tokenizer/__init__.py
@@ -1,1 +1,11 @@
+# char_tokenizer/__init__.py
+from .tokenizer import CharTokenizer
+from .vocab import build_vocab_from_files, collect_texts_from_path, load_vocab, save_vocab
 
+__all__ = [
+    "CharTokenizer",
+    "build_vocab_from_files",
+    "collect_texts_from_path",
+    "load_vocab",
+    "save_vocab",
+]
